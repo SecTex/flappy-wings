9d5e4f3322650afcb2e85afb35d2fddc8d55c5b5
Maik Stegemann <maik-stegemann@protonmail.com>
2023-07-14 15:36:33 +0200

    fix: update canvas size on orientationchange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { fadeInOut } from './animations/fade-in-out.animation';
 
 @Component({
@@ -7,7 +7,7 @@ import { fadeInOut } from './animations/fade-in-out.animation';
   styleUrls: ['./app.component.scss'],
   animations: [fadeInOut('0.3s ease-in', '0.3s ease-out', true)],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   hasInteracted: boolean = false;
 
   canvasWidth: number = window.innerWidth > 0 ? window.innerWidth : screen.width;
@@ -15,11 +15,24 @@ export class AppComponent {
 
   @HostListener('window:resize')
   onResize() {
-    this.canvasWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
-    this.canvasHeight = window.innerHeight > 0 ? window.innerHeight : screen.height;
+    this.updateCanvasSize();
+  }
+
+  @HostListener('window:orientationchange')
+  onOrientationChange() {
+    this.updateCanvasSize();
+  }
+
+  ngOnInit(): void {
+    this.updateCanvasSize();
   }
 
   onInteract(): void {
     this.hasInteracted = true;
+  }
+
+  private updateCanvasSize(): void {
+    this.canvasWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
+    this.canvasHeight = ((window.innerHeight > 0 ? window.innerHeight : screen.height));
   }
 }
